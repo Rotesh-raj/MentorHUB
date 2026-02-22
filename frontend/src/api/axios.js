a5a88d89c0409806e02bfc81c6504c144d27ea46
Ritesh Raj <12345678+Rotesh-raj@users.noreply.github.com>
2026-02-22 23:49:36 +0530

    Fix double https in production URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://https://dsi-connection-1.onrender.com/api",
+  baseURL: "https://dsi-connection-1.onrender.com/api",
   withCredentials: true
 });
 

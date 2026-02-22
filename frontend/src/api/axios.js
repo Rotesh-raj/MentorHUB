0035923c90f9658bb8433858f2c13f1bd9fa64ec
Ritesh Raj <12345678+Rotesh-raj@users.noreply.github.com>
2026-02-22 23:00:16 +0530

    Fix production backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dsi-connection.onrender.com/api",
+  baseURL: "https://https://dsi-connection-1.onrender.com/api",
   withCredentials: true
 });
 
